6730ea522e3bf1e07ea2dd333f6e6be12881e8c6
yohfee <yohfee@gmail.com>
2023-02-12 16:00:20 +0900

    Refine C - Rally

--- abc156_c.fsx
+++ abc156_c.fsx
@@ -4,11 +4,16 @@ let X = stdin.ReadLine().Split " " |> Array.map int
 let min = Array.min X
 let max = Array.max X
 
+let sum =
+    let xx = X |> Array.fold (fun a b -> a + pown b 2) 0
+    let x = X |> Array.sum
+    fun p -> xx + N * pown p 2 - 2 * p * x
+
 let rec loop p v =
     if p > max then
         v
     else
-        let v' = X |> Array.fold (fun a i -> a + pown (i - p) 2) 0
+        let v' = sum p
         loop (p + 1) (if v < v' then v else v')
 
 loop min Core.int.MaxValue |> printfn "%d"
