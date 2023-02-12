ef1b9a46861c50e414d57f0501e299bc50efc43d
yohfee <yohfee@gmail.com>
2023-02-12 19:32:51 +0900

    Refine B - Qualification simulator

--- codefestival_2016_qualB_b.fsx
+++ codefestival_2016_qualB_b.fsx
@@ -1,14 +1,14 @@
 let [| N; A; B |] = stdin.ReadLine().Split " " |> Array.map int
 let S = stdin.ReadLine()
 
-let rec loop t b i =
+let rec loop a b i =
     if i >= N then
         []
     else
         match S.[i] with
-        | 'a' when t < A + B -> 1 :: loop (t + 1) b (i + 1)
-        | 'b' when t < A + B && b < B -> 1 :: loop (t + 1) (b + 1) (i + 1)
-        | _ -> 0 :: loop t b (i + 1)
+        | 'a' when a + b < A + B -> 1 :: loop (a + 1) b (i + 1)
+        | 'b' when a + b < A + B && b < B -> 1 :: loop a (b + 1) (i + 1)
+        | _ -> 0 :: loop a b (i + 1)
 
 loop 0 0 0
 |> List.iter (fun i -> printfn (if i = 1 then "Yes" else "No"))
