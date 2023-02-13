d6b6af78edb8303ae8e3157e7e902dfa666a4357
yohfee <yohfee@gmail.com>
2023-02-13 20:20:15 +0900

    Refine B - Bishop

--- panasonic2020_b.fsx
+++ panasonic2020_b.fsx
@@ -3,6 +3,5 @@ let [| H; W |] = stdin.ReadLine().Split " " |> Array.map int64
 (if H = 1L || W = 1L then
      1L
  else
-     (W / 2L + W % 2L) * (H / 2L + H % 2L)
-     + (W / 2L) * (H / 2L))
+     H * W / 2L + H * W % 2L)
 |> printfn "%d"
