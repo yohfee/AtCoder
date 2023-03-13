c8aaa390e38851f119d3d814f375282731d5a5c6
yohfee <yohfee@gmail.com>
2023-03-13 21:04:00 +0900

    B - Palindromic Numbers

---
+++ abc090_b.fsx
@@ -0,0 +1,10 @@
+let [| A; B |] = stdin.ReadLine().Split " " |> Array.map int
+
+let rec loop i =
+    if i = B + 1 then
+        0
+    else
+        let s = i.ToString().ToCharArray()
+        (if s = Array.rev s then 1 else 0) + loop (i + 1)
+
+loop A |> printfn "%d"
